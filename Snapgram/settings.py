31253c7514ee4f6f9ec54e1394e643b840da2d74
default-007 <brianokola@gmail.com>
2020-05-30 14:58:56 +0300

    Connected app to postgresand achanged the project database settings

--- Snapgram/settings.py
+++ Snapgram/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'Snapgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'snapgram',
+        'USER': 'default-007',
+    'PASSWORD':'expandebles7',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
