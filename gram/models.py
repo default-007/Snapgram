3912707e48de94d300da6216a2f1ed8ec4b21fd3
default-007 <brianokola@gmail.com>
2020-05-31 12:35:56 +0300

    Create the Comments model

--- gram/models.py
+++ gram/models.py
@@ -74,4 +74,14 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
 
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
 
+    def __str__(self):
+        return f'{self.user.name} Post'
+
+    class Meta:
+        ordering = ["-pk"]
