9518836a24c2255a8a4af09c45a55fdd9147918c
default-007 <brianokola@gmail.com>
2020-05-31 08:54:20 +0300

    Create user profile model to store the profile pics

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+    profile_pic = ImageField(blank=True, manual_crop="800x800")
