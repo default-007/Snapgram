e74695b48863a66f6a1729d280ed2605afe2fbf7
default-007 <brianokola@gmail.com>
2020-05-31 12:41:39 +0300

    Test the save and delete function of the Post model

--- gram/tests.py
+++ gram/tests.py
@@ -19,3 +19,23 @@ class TestProfile(TestCase):
         self.profile_test.save_profile()
         after = Profile.objects.all()
         self.assertTrue(len(after) > 0)
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='charles', user=User(username='mikey'))
+        self.profile_test.save()
+
+        self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_insatance(self):
+        self.assertTrue(isinstance(self.image_test, Post))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Post.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
