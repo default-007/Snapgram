822742b6703e8f782982fbfb6f6c72b44f6cb2dc
default-007 <brianokola@gmail.com>
2020-05-31 16:26:16 +0300

    Function to display the no. of likes

--- gram/views.py
+++ gram/views.py
@@ -154,3 +154,24 @@ class PostLikeAPIToggle(APIView):
         }
         print(data)
         return Response(data)
+
+
+def like_post(request):
+    # image = get_object_or_404(Post, id=request.POST.get('image_id'))
+    image = get_object_or_404(Post, id=request.POST.get('id'))
+    is_liked = False
+    if image.likes.filter(id=request.user.id).exists():
+        image.likes.remove(request.user)
+        is_liked = False
+    else:
+        image.likes.add(request.user)
+        is_liked = False
+
+    params = {
+        'image': image,
+        'is_liked': is_liked,
+        'total_likes': image.total_likes()
+    }
+    if request.is_ajax():
+        html = render_to_string('gram_temp/like_section.html', params, request=request)
+        return JsonResponse({'form': html})
