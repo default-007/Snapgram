62d0c80e3cc59d24956c895627b0370d394dba00
default-007 <brianokola@gmail.com>
2020-05-31 15:53:10 +0300

    update user profile page to display bio

--- Snapgram/settings.py
+++ Snapgram/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gram',
     'bootstrap4',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
