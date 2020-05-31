4409e12dd9e92800e46422ffc83e11c47542b2fb
default-007 <brianokola@gmail.com>
2020-05-31 13:44:31 +0300

    Created comment form to write forms

--- gram/forms.py
+++ gram/forms.py
@@ -27,3 +27,13 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('image', 'caption')
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
