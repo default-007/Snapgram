2a166060dc5cca9000ee4f2f2d06fe77c88caa39
default-007 <brianokola@gmail.com>
2020-05-31 12:53:22 +0300

    Created the sign up form

--- gram/forms.py
+++ gram/forms.py
@@ -3,3 +3,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
