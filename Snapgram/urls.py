d6ea15d3308b907466dba80476f14e238a48c45f
default-007 <brianokola@gmail.com>
2020-05-31 15:53:22 +0300

    update user profile page to display bio

--- Snapgram/urls.py
+++ Snapgram/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
-from django.conf.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('', include('gram.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('gram.urls'))
 ]
