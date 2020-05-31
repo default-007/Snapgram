58cf964ad0528d89c216a44d988b504b8808a8e0
default-007 <brianokola@gmail.com>
2020-05-31 12:37:22 +0300

    Create the Follwers model

--- gram/models.py
+++ gram/models.py
@@ -78,3 +78,10 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ["-pk"]
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
