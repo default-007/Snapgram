8cba875263ad9cd17c3ebc65f5e2ecf2726aea66
default-007 <brianokola@gmail.com>
2020-05-31 16:10:32 +0300

    modify settings to display images

--- Snapgram/settings.py
+++ Snapgram/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -135,8 +136,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'index'
 
--- gram/urls.py
+++ gram/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('user_profile/<username>/', views.user_profile, name='user_profile'),
     path('post/<id>', views.post_comment, name='comment'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
