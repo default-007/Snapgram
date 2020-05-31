8dda76bad49463b9facf553e40a094bec59ee5d4
default-007 <brianokola@gmail.com>
2020-05-31 12:40:02 +0300

    Test the the save function of the user model

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Profile, Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='charles')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, name='image', profile_picture='default.jpg', bio='this is a test profile',
+                                    user=self.user)
+    #Test Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    #Test Save Profile
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
