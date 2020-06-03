f1bfb1304ec21765ae061ee573c1394970c2381d
default-007 <brianokola@gmail.com>
2020-06-03 23:49:14 +0300

    Added font styles and color to beautify the page

--- gram/views.py
+++ gram/views.py
@@ -7,9 +7,6 @@ from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.views.generic import RedirectView
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
 
 # Create your views here.
 def signup(request):
